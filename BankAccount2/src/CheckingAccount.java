6688a71f8810688792399ea951bbbc43599f642e
KAU <KAU@KAU-PC>
2017-03-29 17:44:19 +0900

    4 week

---
+++ BankAccount2/src/CheckingAccount.java
@@ -0,0 +1,57 @@
+
+public class CheckingAccount extends Account {
+	
+	private double balance;
+	private double limit;
+	private double interest;
+	private double loaninterest;
+	
+	public CheckingAccount(double balance,double limit,double interest,double loaninterest){
+		this.balance = balance;
+		this.limit = limit;
+		this.interest = interest;
+		this.loaninterest = loaninterest;
+	}
+	
+	@Override
+	double getWithdrawableAccount() {
+		if(balance + limit > 0){
+			return balance + limit;
+		}else{
+			return 0;
+		}
+	}
+
+	public boolean isBankrupted(){
+		if(balance + limit > 0){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	@Override
+	void passTime(int time) {
+		if(balance>0){
+			double sum = balance + time * interest * balance;
+			balance = sum;
+		}else{
+			double sum = balance + time * loaninterest * balance;
+			balance = sum;
+		}
+	}
+	//=========================================== 원래 존재하는 함수
+	
+	public double getBalance(){ //밸런스를 출력
+		return balance;
+	}
+	public void debit(double debit){ //금액 감소
+		if((balance+limit)>debit){
+			balance = balance - debit;			
+		}else{
+			System.out.println("Debit amount exceeded account balance");
+			balance = balance - debit;
+		}
+	}
+	
+}
---
+++ BankAccount2/src/SavingAccount.java
@@ -0,0 +1,45 @@
+
+public class SavingAccount extends Account {
+
+	private double balance;
+	private double interest;
+	private int timeGone;
+	private double sum;
+	
+	public SavingAccount(double balance,double interest){
+		this.balance = balance;
+		this.interest = interest;
+		timeGone = 0;
+		sum = this.balance;
+	}
+	
+	@Override
+	double getWithdrawableAccount() {
+		return balance;
+	}
+
+	@Override
+	void passTime(int time) {
+		if(timeGone<12){
+			sum = sum * Math.pow((1+interest),time);
+			timeGone += time;	
+		}else{
+			balance = sum; 
+			timeGone += time;
+		}
+	}
+	
+	
+	//============================
+	
+	public void debit(double debit){
+		if(timeGone>=12){
+			balance = balance - debit;
+		}else{
+			System.out.println("아직 출금할 수 없습니다.");
+		}
+	}
+	public double getBalance(){ //밸런스를 출력
+		return balance;
+	}
+}
