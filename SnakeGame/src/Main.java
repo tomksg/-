d8bf3c61365543d14938c0d3706b2a254991b502
KAU <KAU@KAU-PC>
2017-05-30 23:53:10 +0900

    Main

--- SnakeGame/src/Main.java
+++ SnakeGame/src/Main.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
 public class Main {
+	public static void main(String args[]) {
+		int total = 0;
+		String key;
+		Scanner scan = new Scanner(System.in);
+		Snakegame game = new Snakegame();
+		// Game start menu
+		while (true) {
 
+			System.out.println("press any key to start");
+
+			key = scan.nextLine();
+			// if(window.event.keyCode==27) -> keylistener 를 사용해서 받을예정
+			if (key.equals('x')) {
+				break;
+			}else{
+				game.Initiategame();
+			}
+			
+		}
+
+	}
 }
---
+++ SnakeGame/src/Snakegame.java
@@ -0,0 +1,6 @@
+
+public class Snakegame {
+	public void Initiategame(){
+		
+	}
+}
