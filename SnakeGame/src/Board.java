d736031834ae9a42e8a5bad73b4b679acd1a44e0
KAU <KAU@KAU-PC>
2017-05-31 00:01:18 +0900

    예전에 보았던 Good JAVA 책 참조

--- SnakeGame/src/Board.java
+++ SnakeGame/src/Board.java
@@ -3,15 +3,26 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+//게임 보드
 public class Board extends JPanel implements ActionListener {
 
 	private final int B_WIDTH = 300;
 	private final int B_HEIGHT = 300;
 
 	//방향키는 4방향으로 유지하는것
+	public Board(){
+		//addKeyListener(); (방향키와 esc의 입력을 통해 잡을것임)
+		setBackground(Color.black);
+		setFocusable(true);
+		
+		setPreferredSize(new Dimension(B_WIDTH,B_HEIGHT));
+		
+	}
 }
